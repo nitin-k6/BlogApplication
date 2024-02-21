96c775cf43265b5c1990a55ac07988eab83814e1
Nitin Kumar <82951031+nitin-k6@users.noreply.github.com>
2024-02-21 22:41:12 +0530

    Update Register.jsx

--- client/src/pages/login/register/Register.jsx
+++ client/src/pages/login/register/Register.jsx
@@ -1,73 +1,62 @@
 import "./register.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+   const [username, setUsername] = useState("")
+   const [email, setEmail] = useState("")
+   const [password, setPassword] =useState("")
+   const [error, setError] = useState(false)
 
-  const handleSubmit = async (e) => {
+   const handleSubmit = async (e) =>{
     e.preventDefault();
     setError(false);
-    try {
-      const res = await axios.post("http://localhost:3000/register", {
+    try{
+      const res = await axios.post("/register", {
         username,
         email,
         password,
       });
-      res.data && window.location.replace("/login");
-      navigate("/login");
-    } catch (err) {
-      setError(true);
+      res.data && window.location.replace("/login")
+    }catch(err){
+       setError(true);
     }
-  };
+   };
 
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerform" onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input
-          type="text"
+        
+        <form className="registerform"  onSubmit={handleSubmit}>
+        <label  >Username</label>
+        <input type="text"
           className="registerInput"
-          placeholder="Enter your username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
+           placeholder="Enter your username"
+           onChange={e => setUsername(e.target.value)}
+           />
 
-        <label>Email</label>
-        <input
-          type="text"
-          className="registerInput"
+        <label >Email</label>
+        <input type="text"
+          className="registerInput" 
           placeholder="Enter your email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password</label>
+          onChange={e => setEmail(e.target.value)}
+          />
+        <label >Password</label>
 
-        <input
-          type="text"
-          className="registerInput"
+        <input type="text"
+         className="registerInput"
           placeholder="Enter your passowrd"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="registerButton" type="submit">
-          Register
+          onChange={e => setPassword(e.target.value)}
+          />
+        <button className="registerButton" type="submit">Register</button>
+        </form>
+
+        <button className="registerLogin">
+          <Link to="/login" style={{textDecoration:"none", color:"inherit"}}>Login</Link>
         </button>
-      </form>
-      <button className="registerLogin">
-        <Link to="/login" style={{ textDecoration: "none", color: "inherit" }}>
-          Login
-        </Link>
-      </button>
-      {error && (
-        <span style={{ color: "red", marginTop: "10px" }}>
-          Something went wrong!
-        </span>
-      )}{" "}
-      {/*This should only be shown when register with same details done next time so we will be using setError(false) below prevent  and {error && in span} */}
-    </div>
-  );
+       {error && <span style={{color: "red", marginTop :"10px"}}>Something went wrong!</span>}  {/*This should only be shown when register with same details done next time so we will be using setError(false) below prevent  and {error && in span} */}
+        </div>
+  )
 }
+
